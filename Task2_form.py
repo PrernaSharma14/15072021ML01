516e5a9cbac092c560cd715b694081f2aa8a745c
PrernaSharma14 <86961309+PrernaSharma14@users.noreply.github.com>
2021-07-29 20:23:18 +0530

    Add files via upload

--- Task2_form.py
+++ Task2_form.py
@@ -2,21 +2,26 @@ import tkinter as tk
 import csv
 app = tk.Tk(__name__)
 app.title('Registration Form')
-app.geometry('250x300') 
+app.geometry('250x300')
 name = tk.Variable(app)
 name.set('')
 email = tk.Variable(app)
 email.set('')
 phn = tk.Variable(app)
 phn.set('')
-f = open("reg_form","w")
-f.close()
-with open("reg_form.csv","a") as f:
-        f.write("Name ,")
-        f.write("Email id ,")
-        f.write("Phone no. , \n")
-        
-        f.close()
+
+tk.Label(app, text = "Form", font = ("Arial",15)).place(x=90, y=30)
+tk.Label(app, text = "Name : ").place(x=25, y=70)
+tk.Entry(app,textvariable = name).place(x=90,y=70)
+tk.Label(app, text = "Email id : ").place(x=25, y=100)
+tk.Entry(app, textvariable = email).place(x=90,y=100)
+tk.Label(app, text = "Phone no. : ").place(x=25, y=130)
+tk.Entry(app, textvariable = phn).place(x=90,y=130)
+
+with open("reg_form","w") as f:
+    datawriter=csv.writer(f)
+    datawriter.writerow(["Name","Email id","Phone no."])
+    f.close()
 def click():
     with open("reg_form.csv","a") as f:
         f.write(name.get())
@@ -30,12 +35,7 @@ def click():
     name.set('')
     email.set('')
     phn.set('')
-tk.Label(app, text = "Sign Up", font = ("Arial",15)).place(x=80, y=30)
-tk.Label(app, text = "Name : ").place(x=25, y=70)
-tk.Entry(app,textvariable = name).place(x=90,y=70)
-tk.Label(app, text = "Email id : ").place(x=25, y=100)
-tk.Entry(app, textvariable = email).place(x=90,y=100)
-tk.Label(app, text = "Phone no. : ").place(x=25, y=130)
-tk.Entry(app, textvariable = phn).place(x=90,y=130)
+
 tk.Button(app,text = "Submit", command = click).place(x=25,y=200)
+
 app.mainloop()
